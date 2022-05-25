fae8b171dac45f1fe597ad660e82a82c3eaf653f
phukuku <85676291+phukuku@users.noreply.github.com>
2022-05-25 17:32:07 +0700

    lam xong login

--- app/src/main/java/com/thellog/laravelapiandroid/Constant.java
+++ app/src/main/java/com/thellog/laravelapiandroid/Constant.java
@@ -1,7 +1,7 @@
 package com.thellog.laravelapiandroid;
 
 public class Constant {
-    public static final String URL= "http://192.168.1.5/";
+    public static final String URL= "http://192.168.1.4/";
     public static final String HOME = URL + "api";
     public static final String LOGIN = HOME + "/login";
     public static final String REGISTER = HOME + "/register";
--- app/src/main/java/com/thellog/laravelapiandroid/MainActivity.java
+++ app/src/main/java/com/thellog/laravelapiandroid/MainActivity.java
@@ -10,55 +10,18 @@ import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                isFirstTime();
+                Intent intent= new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
             }
-        }, 1500);
-
+        },3000);
     }
-
-    private void isFirstTime() {
-        //for checking if the app is running for the very first time
-        //ve need to save a value to shared preferense
-        SharedPreferences preferences = getApplication().getSharedPreferences("onBorad" , Context.MODE_PRIVATE);
-        boolean isFirsTime = preferences.getBoolean("isFirstTime", true);
-        //default value true
-        if(isFirsTime){
-            //if its true then its first time and ve vill change is false
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putBoolean("isFirstTime" , false);
-            editor.apply();
-            //start Onboard activity
-            startActivity(new Intent( MainActivity.this , OnboardActivity.class));
-            finish();
-        }
-        else {
-            //start Auth Activity
-            startActivity(new Intent( MainActivity.this , AuthActivity.class));
-            finish();
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
